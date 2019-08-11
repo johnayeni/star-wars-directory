448eaa602c02c75c630abb5910b805ef2e6e10bd
John Ayeni <johnayeni10.aj@gmail.com>
2019-08-11 08:01:28 +0100

    chore: add vue config file to overide default pwa config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    appleMobileWebAppStatusBarStyle: '#ffe545',
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'images/icons/icon-152x152.png',
+      msTileImage: 'images/icons/msapplication-icon-144x144.png',
+    },
+  },
+};
