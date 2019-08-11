a1f2024f3e93d78480128f15105214952b535757
John Ayeni <johnayeni10.aj@gmail.com>
2019-08-11 13:19:01 +0100

    chore: remove vue persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexPersist from 'vuex-persist';
 import state from './state';
 import mutations from './mutations';
 import getters from './getters';
@@ -8,15 +7,9 @@ import actions from './actions';
 
 Vue.use(Vuex);
 
-const vuexLocalStorage = new VuexPersist({
-  key: 'vuex',
-  storage: window.localStorage,
-});
-
 export default new Vuex.Store({
   state,
   mutations,
   getters,
   actions,
-  plugins: [vuexLocalStorage.plugin],
 });
